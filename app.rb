e88c833486d900f95c94a16170ef973f35e84ddd
martaf793 <martaf@uchicago.edu>
2025-04-17 21:20:17 +0000

    home

--- app.rb
+++ app.rb
@@ -1,11 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erate_list_url="https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_KEY") 
+  # erate_convert_url="https://api.exchangerate.host/convert?from=" + currency_o + "&to=" currency_d "&amount=1&access_key=" + ENV.fetch("EXCHANGE_KEY")"
+  #pp erate_list_url
+  erate_list_api=HTTP.get(erate_list_url).to_s
+  #pp erate_list_api
+  parse_erate_l_body = JSON.parse(erate_list_api)
+  # pp parse_erate_l_body.keys
+  currencies=parse_erate_l_body.fetch("currencies")
+  @list_currencies = currencies.keys
+  # pp currencies.class
   erb(:home)
 end
-#ENV.fetch(exchange_key)
+get("/:currency_o") do
+  @currency_o=params.fetch("currency_o")
+  erb(:origin)
+end
+# get(":currency_o/:currency_d") do
+#   @currency_o=params.fetch("currency_o")
+#   @currency_d=params.fetch("currency_d")
+#   erb(:conversion)
+# end
